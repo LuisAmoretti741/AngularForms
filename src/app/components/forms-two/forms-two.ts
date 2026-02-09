69e6672ec0baa990a76e72f93bad0a23d0f6a902
LuisAmoretti741 <amorettiysrael3@gmail.com>
2026-02-09 13:26:36 +0100

    form 2 ng model

--- src/app/components/forms-two/forms-two.ts
+++ src/app/components/forms-two/forms-two.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms-two',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './forms-two.html',
   styleUrl: './forms-two.scss',
 })
 export class FormsTwo {
+  
+  name: string = 'paperino'
+  surname: string = 'paperone'
+  age: number = 12
+  
+  look() {
+    console.log(this.name);
+    console.log(this.surname);
+    console.log(this.age);
+  }
+
+  changeName(){
+    this.name = 'paperinik'
+  }
+
+  submit(){
+
+  }
 
 }
