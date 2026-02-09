9248235afff186cc529944702447a2f7d8e8f33d
LuisAmoretti741 <amorettiysrael3@gmail.com>
2026-02-09 11:30:49 +0100

    aaaaaa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FormsOne } from './components/forms-one/forms-one';
+import { FormsTwo } from './components/forms-two/forms-two';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo:"/form1", pathMatch:"full"},
+    {path:"form1", component: FormsOne},
+    {path:"form2", component:FormsTwo},
+    {path:"**", redirectTo:"/form1", pathMatch:"full"}
+];
--- src/app/components/forms-one/forms-one.ts
+++ src/app/components/forms-one/forms-one.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms-one',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms-one.html',
   styleUrl: './forms-one.scss',
 })
 export class FormsOne {
 
+  // profileForm = new FormGroup({
+  //   name: new FormControl("pippo"),
+  //   surname: new FormControl("de pippis"),
+  //   adress: new FormGroup({
+  //     city: new FormControl("topolinia"),
+  //     street: new FormControl("corso perrone"),
+  //     number: new FormControl(12)
+  //   })
+  // })
+  fb = inject(FormBuilder);
+
+  profileForm = this.fb.group({
+    name: ['paperone', Validators.required],
+    surname: ['de paperis', Validators.required],
+    adress: this.fb.group({
+      city: ['paperopoli'],
+      street: ['via delle oche'],
+      number: [12]
+    })
+  })
+
+  submit(){
+    console.log(this.profileForm.value);
+  }
 }
